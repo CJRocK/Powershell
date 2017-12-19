55ff6b9ba1de41d4db3cd236ea86abc92137d38a
CJRocK <chetanjoshi1995@gmail.com>
2017-12-19 11:21:27 +0530

    Add files via upload

---
+++ access.ps1
@@ -0,0 +1,1 @@
+powershell -ExecutionPolicy Unrestricted (Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices -Filter TerminalName='RDP-tcp').SetUserAuthenticationRequired(0)
